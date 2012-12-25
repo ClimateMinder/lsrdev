5ddf23bc03e9650a8d6049cca6d8a065079427af
Eric B. Decker <cire831@gmail.com>
2012-12-25 03:14:47 -0800

    add more detail about lsrdev platform

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ lisa
 ====
 
 Lisa working Repository
+
+
+Platforms:
+    Lisa	main platform
+    lsrdev	lsr module (5437/2520/2591) dev board
+
+See doc/00_README for how to set these trees up.
--- lisa.sh
+++ lisa.sh
@@ -1,3 +1,10 @@
-#Replace with your absolute path to the root of lisa tree
+# Replace with your absolute path to the root of lisa tree
+# tp-master tree is at $HOME/w/t2_cur/tinyos-2.x
 export LISA_ROOT="${HOME}/w/lisa"
 export TOSMAKE_PATH="$TOSMAKE_PATH $LISA_ROOT/support/make"
+
+export TOSROOT=$HOME/w/t2_cur/tinyos-2.x
+export TOSDIR=$TOSROOT/tos
+export MAKERULES=$TOSROOT/support/make/Makerules
+export CLASSPATH=.:$TOSROOT/support/sdk/java/tinyos.jar
+export PYTHONPATH=$TOSROOT/support/sdk/python:$PYTHONPATH
